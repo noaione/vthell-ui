b4bca5784c411396b9e61fbd904ef0e4fba90b9b
noaione <noaione0809@gmail.com>
2021-05-26 22:21:30 +0700

    frontend(pages): add custom 404 page

---
+++ frontend/pages/404.tsx
@@ -0,0 +1,32 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+import React from "react";
+
+import BaseContainer from "../components/BaseContainer";
+import MetadataHead from "../components/MetadataHead";
+import Navbar from "../components/Navbar";
+
+export default function NotFoundPage() {
+    const router = useRouter();
+
+    return (
+        <>
+            <Head>
+                <MetadataHead.Base />
+                <title>404 :: VTHell WebUI</title>
+                <MetadataHead.SEO />
+                <MetadataHead.Prefetch />
+            </Head>
+            <Navbar mode="error" />
+            <main className="antialiased h-full pb-4">
+                <BaseContainer className="flex flex-col gap-4 mt-8 mb-6 text-center" removeShadow>
+                    <span className="text-2xl font-light mx-2">Link Not found :(</span>
+                    <div className="mx-2 justify-center">
+                        <code className="text-lg text-gray-400">{router.route}</code>
+                        <span className="ml-1">is not a valid link</span>
+                    </div>
+                </BaseContainer>
+            </main>
+        </>
+    );
+}
