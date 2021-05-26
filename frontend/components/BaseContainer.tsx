c927ab8925d6069e1a0e332502c048dd935a97a0
noaione <noaione0809@gmail.com>
2021-05-26 22:09:04 +0700

    frontend(BaseContainer): add removeShadow option

--- frontend/components/BaseContainer.tsx
+++ frontend/components/BaseContainer.tsx
@@ -3,9 +3,11 @@ import React from "react";
 import { isType } from "../lib/utils";
 
 export default function BaseContainer(
-    props: React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>
+    props: React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> & {
+        removeShadow?: boolean;
+    }
 ) {
-    const { children, className, ...extracted } = props;
+    const { children, className, removeShadow, ...extracted } = props;
 
     let extraClass = "";
     if (isType(className, "string")) {
@@ -13,7 +15,10 @@ export default function BaseContainer(
     }
 
     return (
-        <div {...extracted} className={`w-full m-auto md:w-2/3 shadow-md rounded-lg ${extraClass}`}>
+        <div
+            {...extracted}
+            className={`w-full m-auto md:w-2/3 ${!removeShadow ? "shadow-md" : ""} rounded-lg ${extraClass}`}
+        >
             {children}
         </div>
     );
